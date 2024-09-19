7400973549697ca189bdfaddf0a986e8d1bddea4
Khushi Tolani <137473049+kkhushitolanii@users.noreply.github.com>
2024-09-19 16:04:30 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-// Background script, if needed for future enhancements
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'statusUpdate') {
       chrome.storage.local.set({ addedCount: message.addedCount });
